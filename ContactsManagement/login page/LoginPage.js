ca967829aad5800bd7a8b5382d6062e56e56ebb7
unknown <fareedtijani2810@gmail.com>
2024-08-14 07:03:09 +0100

    sign-up page is connected to the back end api

---
+++ ContactsManagement/login page/LoginPage.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".form-tag")
+const url = "http://localhost:8080/api/v1/user/login"
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const password = document.querySelector("#password").value
+    const email = document.querySelector("#email").value
+    const data = await fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email: email, password: password})
+    })
+    if(data.ok){
+        window.location.href = "./ContactsFrontEnd.html";
+    } else {
+        alert("Invalid email or password")
+    }
+})
+
+
+
