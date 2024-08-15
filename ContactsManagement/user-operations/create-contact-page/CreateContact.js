9550746e7cbf576087ea12fe19fc2fbf75380cea
unknown <fareedtijani2810@gmail.com>
2024-08-15 01:48:45 +0100

    app-sucessfully-creates-new-contact

---
+++ ContactsManagement/user-operations/create-contact-page/CreateContact.js
@@ -0,0 +1,31 @@
+const form = document.querySelector(".form-tag");
+const url = "http://localhost:8080/api/v1/user/create-contact"
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const firstName = document.querySelector("#first-name").value
+    const lastName = document.querySelector("#last-name").value
+    const phoneNumber = document.querySelector("#phone-number").value
+    const email = document.querySelector("#email").value
+    const address = document.querySelector("#address").value
+    const ownerEmail = document.querySelector("#owner-email").value
+
+    const data = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, email: email, address: address, ownerEmail: ownerEmail}),
+    })
+    if (data.ok) {
+        const responseData = await data.json(); // Get response data
+        console.log(responseData); // Log response data
+        alert("Contact created successfully!");
+        window.location.href = "../UserOperations.html";
+    } else {
+        const errorMessage = await data.text(); // Get error message
+        console.error(errorMessage); // Log error message
+        alert("Failed to create contact!");
+    }
+});
+
